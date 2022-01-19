66bd828adfded81f3e0b58de8cf4acb6e4de8022
Gonzalo <bravomoyanogonzalo@gmail.com>
2022-01-19 20:29:39 +0100

    .

--- configurar_maquinas.sh
+++ configurar_maquinas.sh
@@ -9,22 +9,22 @@ then
 fi
 
 read -p "¿Cual es el nombre de la red de tu proyecto en OpenStack? " red_proyecto
-read -p "¿Cual es la IP privada de esta maquina dentro en openstack?" ip_privada
+read -p "¿Cual es la IP privada de esta maquina dentro en openstack? " ip_privada
 read -p "¿Cuantas MV quieres? " num
 
 for i in $(seq 1 $num)
 do
-  sudo docker-machine create -d openstack --openstack-flavor-name medium --openstack-image-name "Ubuntu 20" --openstack-domain-name default --openstack-net-name $red_proyecto --openstack-floatingip-pool ext-net --openstack-ssh-user ubuntu --openstack-sec-groups cam-stack --openstack-keypair-name cam-stack --openstack-private-key-file /home/ubuntu/.ssh/cam-stack.pem nodo$i
+  docker-machine create -d openstack --openstack-flavor-name medium --openstack-image-name "Ubuntu 20" --openstack-domain-name default --openstack-net-name $red_proyecto --openstack-floatingip-pool ext-net --openstack-ssh-user ubuntu --openstack-sec-groups cam-stack --openstack-keypair-name cam-stack --openstack-private-key-file /home/ubuntu/.ssh/cam-stack.pem nodo$i
 done
 
 for i in $(seq 1 $num)
 do
   echo "Instalando plugin vieux/sshfs en el nodo$i"
-  sudo docker-machine ssh nodo$i "sudo docker plugin install --grant-all-permissions vieux/sshfs"
+  docker-machine ssh nodo$i "sudo docker plugin install --grant-all-permissions vieux/sshfs"
   echo "Creando el volumen de docker en el nodo $i"
-  sudo docker-machine ssh nodo$i "sudo docker volume create --driver vieux/sshfs -o sshcmd=ubuntu@$(ip_privada):/home/ubuntu/volumen -o allow_other -o password=ubuntu volumen-web"
+  docker-machine ssh nodo$i "sudo docker volume create --driver vieux/sshfs -o sshcmd=ubuntu@$(ip_privada):/home/ubuntu/volumen -o allow_other -o password=ubuntu volumen-web"
 done
 
 echo "Instalando docker-compose en el nodo 1"
-sudo docker-machine ssh nodo1 "curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-$x86_64\" -o /usr/local/bin/docker-compose"
-sudo docker-machine ssh nodo1 "sudo chmod +x /usr/local/bin/docker-compose"
+docker-machine ssh nodo1 "curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-$x86_64\" -o /usr/local/bin/docker-compose"
+docker-machine ssh nodo1 "sudo chmod +x /usr/local/bin/docker-compose"
