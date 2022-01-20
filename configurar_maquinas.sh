6c66ff225b7f91397e98a7904a0f88b20a8349c2
Gonzalo <bravomoyanogonzalo@gmail.com>
2022-01-20 11:35:47 +0100

    .

--- configurar_maquinas.sh
+++ configurar_maquinas.sh
@@ -31,5 +31,3 @@ echo "Instalando docker-compose en el nodo 1"
 docker-machine ssh nodo1 "sudo curl -L \"https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64\" -o /usr/local/bin/docker-compose"
 docker-machine ssh nodo1 "sudo chmod +x /usr/local/bin/docker-compose"
 
-echo "\n\n\nAhora reinicia todos los nodos para que el usuario ubuntu pueda utilizar docker."
-echo "Utiliza: docker-machine ssh nodoX \"sudo reboot\""
